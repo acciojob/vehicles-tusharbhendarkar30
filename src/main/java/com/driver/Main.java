7f095ac9cf1a3489a833d49cd3ce997bda19a258
tusharbhendarkar30 <121106268+tusharbhendarkar30@users.noreply.github.com>
2024-09-02 11:03:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,21 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+        
+        Vehicle vehicle = new Vehicle("Generic Vehicle");
+        System.out.println("Vehicle Name: " + vehicle.getVehicleName());
+
+        Car car = new Car("Family Car", 5);
+        System.out.println("Car Name: " + car.getVehicleName());
+        System.out.println("Car Capacity: " + car.getVehicleCapacity());
+
+        F1 f1 = new F1("F1 Racing Car", 2);
+        System.out.println("F1 Car Name: " + f1.getVehicleName());
+        System.out.println("F1 Car Capacity: " + f1.getVehicleCapacity());
+
+       
+        Boat boat = new Boat("Speedy Boat", 10);
+        System.out.println("Boat Name: " + boat.getVehicleName());
+        System.out.println("Boat Capacity: " + boat.getVehicleCapacity());
     }
 }
