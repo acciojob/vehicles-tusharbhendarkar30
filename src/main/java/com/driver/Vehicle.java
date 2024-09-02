3852854d366011fc50fd80ac1eed298e2da402fe
tusharbhendarkar30 <121106268+tusharbhendarkar30@users.noreply.github.com>
2024-09-02 10:58:45 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -2,21 +2,15 @@ package com.driver;
 
 public class Vehicle {
 
-    class Vehicle {
-    protected String name;
-    protected int capacity;
-
-    public Vehicle(String name, int capacity) {
+    String name;
+    
+    public Vehicle(String name) {
         this.name = name;
-        this.capacity = capacity;
     }
-
+    
     public String getVehicleName() {
         return name;
     }
 
-    public int getVehicleCapacity() {
-        return capacity;
-    }
 
 }
